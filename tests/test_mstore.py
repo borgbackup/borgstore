24e3e4f6676979c98f4c6731db562adf836fd775
Thomas Waldmann <tw@waldmann-edv.de>
2024-08-05 12:03:30 +0200

    mstore: add contextmanager

--- tests/test_mstore.py
+++ tests/test_mstore.py
@@ -58,152 +58,150 @@ def test_lookup_bucket(buckets, key, store):
 
 
 @pytest.fixture()
-def mstore_jbod(tmp_path):
+def mstore_jbod_created(tmp_path):
     stores = [Store(url=f"file://{tmp_path}0"), Store(url=f"file://{tmp_path}1")]
     mstore = MStore(stores=stores, buckets=[192, 64], kinds={"": "hex-hash"})
     mstore.create()
-    mstore.open()
     try:
         yield mstore
     finally:
-        mstore.close()
         mstore.destroy()
 
 
 @pytest.fixture()
-def mstore_mirror(tmp_path):
+def mstore_mirror_created(tmp_path):
     stores = [Store(url=f"file://{tmp_path}0"), Store(url=f"file://{tmp_path}1")]
     mstore = MStore(stores=stores, buckets=[256, 256], kinds={"": "hex-hash"})
     mstore.create()
-    mstore.open()
     try:
         yield mstore
     finally:
-        mstore.close()
         mstore.destroy()
 
 
-def fill_storage(store: MStore, count: int, *, start: int = 0) -> MStore:
+def fill_storage(store: MStore, count: int, *, start: int = 0) -> None:
     for i in range(start, start + count, 1):
         k, v = lkey(i), str(i).encode()
         store.store(k, v)
-    return store
 
 
-def read_storage(store: MStore, count: int, *, start: int = 0) -> MStore:
+def read_storage(store: MStore, count: int, *, start: int = 0) -> None:
     # can we still read all data?
     for i in range(start, start + count, 1):
         k, v = lkey(i), str(i).encode()
         assert store.load(k) == v
-    return store
 
 
-def test_list(mstore_mirror):
-    mstore = fill_storage(mstore_mirror, 1024)
-    # there must be no duplication of keys from the mirror mstore
-    assert list_store_names(mstore, "") == sorted([lkey(i) for i in range(1024)])
+def test_list(mstore_mirror_created):
+    with mstore_mirror_created as mstore:
+        fill_storage(mstore, 1024)
+        # there must be no duplication of keys from the mirror mstore
+        assert list_store_names(mstore, "") == sorted([lkey(i) for i in range(1024)])
 
 
-def test_list(mstore_jbod):
-    mstore = fill_storage(mstore_jbod, 1024)
-    # check if we get all expected keys from the jbod mstore
-    assert list_store_names(mstore, "") == sorted([lkey(i) for i in range(1024)])
+def test_list(mstore_jbod_created):
+    with mstore_jbod_created as mstore:
+        fill_storage(mstore, 1024)
+        # check if we get all expected keys from the jbod mstore
+        assert list_store_names(mstore, "") == sorted([lkey(i) for i in range(1024)])
 
 
-def test_load_store_list_distribution(mstore_jbod):
-    mstore = fill_storage(mstore_jbod, 1024)
-    # check if all data is readable and as expected:
-    for i in range(1024):
-        k, v = lkey(i), str(i).encode()
-        assert mstore.load(k) == v
-    # check if data ended up in the stores according to the ratio configured in mstore_jbod (192 : 64)
-    keys_mstore = list_store_names(mstore, "")
-    keys_store0 = list_store_names(mstore.stores[0], "")
-    keys_store1 = list_store_names(mstore.stores[1], "")
-    assert len(keys_mstore) == len(set(keys_mstore)) == 1024
-    assert len(keys_store0) == len(set(keys_store0)) == 768
-    assert len(keys_store1) == len(set(keys_store1)) == 256
+def test_load_store_list_distribution(mstore_jbod_created):
+    with mstore_jbod_created as mstore:
+        fill_storage(mstore, 1024)
+        # check if all data is readable and as expected:
+        for i in range(1024):
+            k, v = lkey(i), str(i).encode()
+            assert mstore.load(k) == v
+        # check if data ended up in the stores according to the ratio configured in mstore_jbod (192 : 64)
+        keys_mstore = list_store_names(mstore, "")
+        keys_store0 = list_store_names(mstore.stores[0], "")
+        keys_store1 = list_store_names(mstore.stores[1], "")
+        assert len(keys_mstore) == len(set(keys_mstore)) == 1024
+        assert len(keys_store0) == len(set(keys_store0)) == 768
+        assert len(keys_store1) == len(set(keys_store1)) == 256
 
 
-def test_load_store_list_redundancy(mstore_mirror):
-    mstore = fill_storage(mstore_mirror, 1024)
-    # delete stuff from store 0:
-    for i in 0, 23, 42, 1001:
-        mstore.stores[0].delete(lkey(i))
-    # check if it is really gone:
-    for i in 0, 23, 42, 1001:
-        with pytest.raises(KeyError):
-            mstore.stores[0].load(lkey(i))
-    # delete other stuff from store 1:
-    for i in 123, 456, 789:
-        mstore.stores[1].delete(lkey(i))
-    # check if it is really gone:
-    for i in 123, 456, 789:
-        with pytest.raises(KeyError):
-            mstore.stores[1].load(lkey(i))
-    # check if we can still read everything from the mirror:
-    for i in range(1024):
-        k, v = lkey(i), str(i).encode()
-        assert mstore.load(k) == v
-    # also check if list still works ok:
-    assert list_store_names(mstore, "") == sorted([lkey(i) for i in range(1024)])
-    # now delete some values also from the other side of the mirror:
-    for i in 0, 23, 42, 1001:
-        mstore.stores[1].delete(lkey(i))
-    for i in 123, 456, 789:
-        mstore.stores[0].delete(lkey(i))
-    # now the mirror is expected to be partially corrupted at these places:
-    for i in 0, 23, 42, 1001, 123, 456, 789:
-        with pytest.raises(KeyError):
-            mstore.load(lkey(i))
-    # list is expected to miss some elements:
-    assert list_store_names(mstore, "") == sorted(
-        [lkey(i) for i in range(1024) if i not in [0, 23, 42, 1001, 123, 456, 789]]
-    )
+def test_load_store_list_redundancy(mstore_mirror_created):
+    with mstore_mirror_created as mstore:
+        fill_storage(mstore, 1024)
+        # delete stuff from store 0:
+        for i in 0, 23, 42, 1001:
+            mstore.stores[0].delete(lkey(i))
+        # check if it is really gone:
+        for i in 0, 23, 42, 1001:
+            with pytest.raises(KeyError):
+                mstore.stores[0].load(lkey(i))
+        # delete other stuff from store 1:
+        for i in 123, 456, 789:
+            mstore.stores[1].delete(lkey(i))
+        # check if it is really gone:
+        for i in 123, 456, 789:
+            with pytest.raises(KeyError):
+                mstore.stores[1].load(lkey(i))
+        # check if we can still read everything from the mirror:
+        for i in range(1024):
+            k, v = lkey(i), str(i).encode()
+            assert mstore.load(k) == v
+        # also check if list still works ok:
+        assert list_store_names(mstore, "") == sorted([lkey(i) for i in range(1024)])
+        # now delete some values also from the other side of the mirror:
+        for i in 0, 23, 42, 1001:
+            mstore.stores[1].delete(lkey(i))
+        for i in 123, 456, 789:
+            mstore.stores[0].delete(lkey(i))
+        # now the mirror is expected to be partially corrupted at these places:
+        for i in 0, 23, 42, 1001, 123, 456, 789:
+            with pytest.raises(KeyError):
+                mstore.load(lkey(i))
+        # list is expected to miss some elements:
+        assert list_store_names(mstore, "") == sorted(
+            [lkey(i) for i in range(1024) if i not in [0, 23, 42, 1001, 123, 456, 789]]
+        )
 
 
-def test_move_delete_undelete(mstore_mirror):
+def test_move_delete_undelete(mstore_mirror_created):
     k0, v0 = key(0), b"value0"
     k1, v1 = key(1), b"value1"
-    store = mstore_mirror
-    store.store(k0, v0)
-    store.store(k1, v1)
-    # delete
-    store.move(k0, delete=True)  # soft delete
-    assert list_store_names(store, "", deleted=False) == [k1]
-    assert list_store_names(store, "", deleted=True) == [k0, k1]
-    # undelete
-    store.move(k0, undelete=True)  # undelete previously soft deleted item
-    assert list_store_names(store, "", deleted=False) == [k0, k1]
-    assert list_store_names(store, "", deleted=True) == [k0, k1]
+    with mstore_mirror_created as mstore:
+        mstore.store(k0, v0)
+        mstore.store(k1, v1)
+        # delete
+        mstore.move(k0, delete=True)  # soft delete
+        assert list_store_names(mstore, "", deleted=False) == [k1]
+        assert list_store_names(mstore, "", deleted=True) == [k0, k1]
+        # undelete
+        mstore.move(k0, undelete=True)  # undelete previously soft deleted item
+        assert list_store_names(mstore, "", deleted=False) == [k0, k1]
+        assert list_store_names(mstore, "", deleted=True) == [k0, k1]
 
 
-def test_namespaces(mstore_jbod):
-    store = mstore_jbod
-    store.kinds = [("config/", "generic"), ("data/", "hex-hash")]
-    store.store("config/main", b"some config")
-    store.store("data/0000", b"value_00")
-    store.store("data/bf00", b"value_bf")
-    store.store("data/c000", b"value_c0")
-    store.store("data/ff00", b"value_ff")
-    # now let's check where stuff ended up being stored.
-    st0, st1 = store.stores
-    # hex-hash kind of data should be spread into buckets according to its hash:
-    assert st0.load("data/0000") == b"value_00"
-    assert st0.load("data/bf00") == b"value_bf"
-    with pytest.raises(KeyError):
-        st0.load("data/c000")
-    with pytest.raises(KeyError):
-        st0.load("data/ff00")
-    with pytest.raises(KeyError):
-        st1.load("data/0000")
-    with pytest.raises(KeyError):
-        st1.load("data/bf00")
-    assert st1.load("data/c000") == b"value_c0"
-    assert st1.load("data/ff00") == b"value_ff"
-    # generic kind config should be mirrored to all stores:
-    assert st0.load("config/main") == b"some config"
-    assert st1.load("config/main") == b"some config"
+def test_namespaces(mstore_jbod_created):
+    with mstore_jbod_created as mstore:
+        mstore.kinds = [("config/", "generic"), ("data/", "hex-hash")]
+        mstore.store("config/main", b"some config")
+        mstore.store("data/0000", b"value_00")
+        mstore.store("data/bf00", b"value_bf")
+        mstore.store("data/c000", b"value_c0")
+        mstore.store("data/ff00", b"value_ff")
+        # now let's check where stuff ended up being stored.
+        st0, st1 = mstore.stores
+        # hex-hash kind of data should be spread into buckets according to its hash:
+        assert st0.load("data/0000") == b"value_00"
+        assert st0.load("data/bf00") == b"value_bf"
+        with pytest.raises(KeyError):
+            st0.load("data/c000")
+        with pytest.raises(KeyError):
+            st0.load("data/ff00")
+        with pytest.raises(KeyError):
+            st1.load("data/0000")
+        with pytest.raises(KeyError):
+            st1.load("data/bf00")
+        assert st1.load("data/c000") == b"value_c0"
+        assert st1.load("data/ff00") == b"value_ff"
+        # generic kind config should be mirrored to all stores:
+        assert st0.load("config/main") == b"some config"
+        assert st1.load("config/main") == b"some config"
 
 
 def test_reduce_prepare(tmp_path):
@@ -216,23 +214,21 @@ def test_reduce_prepare(tmp_path):
     stores = [Store(url=f"file://{tmp_path}0"), Store(url=f"file://{tmp_path}1"), Store(url=f"file://{tmp_path}2")]
     mstore = MStore(stores=stores, buckets=[128, 64, 64], kinds={"": "hex-hash"})
     mstore.create()
-    mstore.open()
-    fill_storage(mstore, entries)
-    read_storage(mstore, entries)
-    assert len(list_store_names(mstore.stores[0], "")) == 512
-    assert len(list_store_names(mstore.stores[1], "")) == 256
-    assert len(list_store_names(mstore.stores[2], "")) == 256
-    mstore.close()
+    with mstore:
+        fill_storage(mstore, entries)
+        read_storage(mstore, entries)
+        assert len(list_store_names(mstore.stores[0], "")) == 512
+        assert len(list_store_names(mstore.stores[1], "")) == 256
+        assert len(list_store_names(mstore.stores[2], "")) == 256
     # test: still have the 3 stores available, but bucket count 0 in store 2 means no new data will go into it:
     stores = [Store(url=f"file://{tmp_path}0"), Store(url=f"file://{tmp_path}1"), Store(url=f"file://{tmp_path}2")]
     mstore = MStore(stores=stores, buckets=[128, 128, 0], kinds={"": "hex-hash"})
-    mstore.open()
-    read_storage(mstore, entries)
-    # store new stuff into the mstore:
-    fill_storage(mstore, entries, start=entries)
-    read_storage(mstore, entries * 2)
-    assert len(list_store_names(mstore.stores[0], "")) == 512 + 512
-    assert len(list_store_names(mstore.stores[1], "")) == 256 + 512
-    assert len(list_store_names(mstore.stores[2], "")) == 256  # no new data was written to store 2
-    mstore.close()
+    with mstore:
+        read_storage(mstore, entries)
+        # store new stuff into the mstore:
+        fill_storage(mstore, entries, start=entries)
+        read_storage(mstore, entries * 2)
+        assert len(list_store_names(mstore.stores[0], "")) == 512 + 512
+        assert len(list_store_names(mstore.stores[1], "")) == 256 + 512
+        assert len(list_store_names(mstore.stores[2], "")) == 256  # no new data was written to store 2
     mstore.destroy()
