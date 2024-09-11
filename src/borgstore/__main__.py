13dd281983fe8ea0f110abe9878959a1f4e455e2
Thomas Waldmann <tw@waldmann-edv.de>
2024-09-11 15:08:11 +0200

    fix demo code, also output stats

--- src/borgstore/__main__.py
+++ src/borgstore/__main__.py
@@ -31,31 +31,34 @@ def run_demo(storage_url):
         print("Error: you must not give an existing directory.")
         return
 
-    print("Writing 2 items to config namespace...")
-    settings1_key = "config/settings1"
-    store.store(settings1_key, b"value1 = 42")
-    settings2_key = "config/settings2"
-    store.store(settings2_key, b"value2 = 23")
+    with store:
+        print("Writing 2 items to config namespace...")
+        settings1_key = "config/settings1"
+        store.store(settings1_key, b"value1 = 42")
+        settings2_key = "config/settings2"
+        store.store(settings2_key, b"value2 = 23")
 
-    print(f"Listing config namespace contents: {list(store.list('config'))}")
+        print(f"Listing config namespace contents: {list(store.list('config'))}")
 
-    settings1_value = store.load(settings1_key)
-    print(f"Loaded from store: {settings1_key}: {settings1_value.decode()}")
-    settings2_value = store.load(settings2_key)
-    print(f"Loaded from store: {settings2_key}: {settings2_value.decode()}")
+        settings1_value = store.load(settings1_key)
+        print(f"Loaded from store: {settings1_key}: {settings1_value.decode()}")
+        settings2_value = store.load(settings2_key)
+        print(f"Loaded from store: {settings2_key}: {settings2_value.decode()}")
 
-    print("Writing 2 items to data namespace...")
-    data1 = b"some arbitrary binary data."
-    key1 = id_key(data1)
-    store.store(key1, data1)
-    data2 = b"more arbitrary binary data. " * 2
-    key2 = id_key(data2)
-    store.store(key2, data2)
-    print(f"Soft deleting item {key2} ...")
-    store.move(key2, delete=True)
+        print("Writing 2 items to data namespace...")
+        data1 = b"some arbitrary binary data."
+        key1 = id_key(data1)
+        store.store(key1, data1)
+        data2 = b"more arbitrary binary data. " * 2
+        key2 = id_key(data2)
+        store.store(key2, data2)
+        print(f"Soft deleting item {key2} ...")
+        store.move(key2, delete=True)
 
-    print(f"Listing data namespace contents: {list(store.list('data', deleted=False))}")
-    print(f"Listing data namespace contents, incl. deleted: {list(store.list('data', deleted=True))}")
+        print(f"Listing data namespace contents: {list(store.list('data', deleted=False))}")
+        print(f"Listing data namespace contents, incl. deleted: {list(store.list('data', deleted=True))}")
+
+        print(f"Stats: {store.stats}")
 
     answer = input("After you've inspected the storage, enter DESTROY to destroy the storage, anything else to abort: ")
     if answer == "DESTROY":
